b6f204e519545215d6f10c3e50ae127a55335616
Sergio Berdeja <sergioberdeja@gmail.com>
2022-07-13 16:17:41 -0600

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+# require 'pry'
 def oxford_comma(array)
+    return array[0] if array.size == 1
+    return array.join(' and ') if array.size == 2
 
+    punctuated_array = array.map.with_index(1) do |word, index|
+        if index == array.size
+            "and #{word}"
+        else
+            "#{word},"
+        end
+    end
+    # binding.pry
+    punctuated_array.join(' ')
 end
+
+# oxford_comma(['carrot', 'peas', 'cheese', 'bees'])
